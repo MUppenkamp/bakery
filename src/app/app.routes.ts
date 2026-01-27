8c2533489471489a176caa98999d87417efd77d0
Mona <35469083+MUppenkamp@users.noreply.github.com>
2026-01-27 01:08:45 +0100

    Add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ProductComponent } from './components/products/product/product.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'Über', component: ProductComponent },
+  { path: 'Produkte', component: ProductComponent },
+  { path: 'Blog', component: ProductComponent },
+  { path: 'Kontakt', component: ProductComponent },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
-import { ProductsComponent } from "./components/products/products.component";
-import { HeroComponent } from "./components/hero/hero.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ProductsComponent, HeroComponent],
+  imports: [HeaderComponent, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('monas-bakery');
-}
+export class App {}
