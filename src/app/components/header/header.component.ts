0d2f3656d1373135fc8a92db421283ae272bcb67
Mona <35469083+MUppenkamp@users.noreply.github.com>
2026-01-27 01:20:42 +0100

    Add header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,4 +5,13 @@ import { Component } from '@angular/core';
   selector: 'app-header',
   templateUrl: 'header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  protected readonly items = [
+    // TODO Maybe an icon to go to homepage
+    { url: '', name: '🤎' },
+    { url: '/Über', name: 'Über mich :)' },
+    { url: '/Produkte', name: 'Produkte' },
+    { url: '/Blog', name: 'Blog' },
+    { url: '/Kontakt', name: 'Kontakt' },
+  ];
+}
