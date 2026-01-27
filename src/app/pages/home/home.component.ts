d2b112c93ca5b0c2f60b3ea06174b530db1d62a7
Mona <35469083+MUppenkamp@users.noreply.github.com>
2026-01-27 01:21:39 +0100

    Remove standalone true

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { HeroComponent } from "../../components/hero/hero.component";
-import { ProductsComponent } from "../../components/products/products.component";
+import { HeroComponent } from '../../components/hero/hero.component';
+import { ProductsComponent } from '../../components/products/products.component';
 
 @Component({
-  standalone: true,
   imports: [HeroComponent, ProductsComponent],
   selector: 'app-home',
   templateUrl: 'home.component.html',
