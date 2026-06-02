f98a008d89baceb03d7cf9a1b312a3305b731105
Mona <35469083+MUppenkamp@users.noreply.github.com>
2026-06-02 23:00:37 +0200

    Add ProductTag and category

--- src/app/types/product.types.ts
+++ src/app/types/product.types.ts
@@ -4,4 +4,17 @@ export type Product = {
   description?: string;
   price: number;
   imageUrl?: string;
+  tags: ProductTag[];
+  category: 'Brot' | 'Kuchen' | 'Gebäck';
 };
+
+/**
+ * Enum representing the tags that can be associated with a product. These tags can be used for categorization and filtering of products in the application.
+ */
+export enum ProductTag {
+  Bread,
+  Cake,
+  Pastry,
+  Vegan,
+  GlutenFree,
+}
