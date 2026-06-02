1446ffa605066462cf8f2243cf3172b6d992ba91
Mona <35469083+MUppenkamp@users.noreply.github.com>
2026-06-02 23:10:51 +0200

    Add pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductsComponent } from './pages/products/products.component';
+import { AboutComponent } from './pages/about/about.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'Über', component: ProductsComponent },
+  { path: 'Über', component: AboutComponent },
   { path: 'Produkte', component: ProductsComponent },
-  { path: 'Blog', component: ProductsComponent },
-  { path: 'Kontakt', component: ProductsComponent },
+  { path: 'Blog', component: BlogComponent },
+  { path: 'Kontakt', component: ContactComponent },
 ];
