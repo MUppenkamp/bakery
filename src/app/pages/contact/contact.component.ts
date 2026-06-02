1a66c08b2f5815ff509e4ae8010682dff1e88622
Mona <35469083+MUppenkamp@users.noreply.github.com>
2026-06-02 23:10:17 +0200

    Add contact

---
+++ src/app/pages/contact/contact.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-contact',
+  templateUrl: 'contact.component.html',
+})
+export class ContactComponent {}
